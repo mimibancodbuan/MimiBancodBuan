8f5a5c8286a56e1938dbb306caecde5085106c1a
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-09-13 10:58:06 -0400

    Completed the Coding Convention Lesson

--- MimiBuan/src/intro/CodingConventions.java
+++ MimiBuan/src/intro/CodingConventions.java
@@ -14,9 +14,21 @@ public class CodingConventions {
 	 * therefore the name of a constructor must always
 	 * match the name of the class
 	 */
-	public CodingConventions() {
-		//In Java, fields are instantiated in a constructor 
-		name = "Mimi";
-		description = "is a student";
+	public CodingConventions(String name, int index) {
+		//In Java, fields are instantiated in a constructor
+		
+		// there is a local variable called 'name' and a field called 'name
+		// the field persists, but the local variable data is destroyed at the end of this method
+		//distinguish the field by using the reserved word "this"
+		
+		this.name = name;
+		// static reference begins with a class name
+		description = IntroMain.DESCRIPTIONS[index];
+	}
+	
+	public void doStuff() {
+		// static method call
+		System.out.println(name + description); //use a space between your operations
+		System.out.print("The square root of 12 is " + Math.sqrt(12));
 	}
 }
--- MimiBuan/src/intro/IntroMain.java
+++ MimiBuan/src/intro/IntroMain.java
@@ -2,12 +2,21 @@ package intro; // this is a package declaration
 
 // this is a class header (class declaration)
 public class IntroMain {
+	
+	// constants (public)
+	public static final String[] DESCRIPTIONS = {" is a teacher",
+			" is a student",
+			" is a human being",
+			" is imaginary"};
 
 	public static void main(String[] args) {
 		// declaration of a local variable
 		// notice the use of the word 'new'
 		// new must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Mimi", 1);
+		
+		//instance method call
+		conventionsInstance.doStuff();
 	}
 
 }
