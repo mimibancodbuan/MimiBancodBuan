53e5b1f6bfc8db9c58f3d167e15a92075d1a4a6b
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-10-20 11:45:07 -0400

    2d arrays

---
+++ MimiBuan/src/arrays2d/Main.java
@@ -0,0 +1,30 @@
+package arrays2d;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+
+	/*
+	 * increases the element in arr at index psn
+	 * and decreases the elements at psn -1 and psn +1
+	 * if they exist PRECONDIDTION: arr[psn] exists
+	 * 
+	 * THIS idea is another major topic in AP CSA.
+	 * It is the idea of avoiding an ArrayIndexOutOfBoundsException
+	 * you will see it on quizzes, tests, and project rubrics
+	 * SO ALWAYS BE MINDFUL!
+	 * @param arr
+	 * @param psn
+	*/
+	public static void changeNeighbors(int[] arr, int psn) {
+		arr[psn] = 
+				
+		
+		for(int i = 0; i < arr.length; i++) {
+			if (psn >= 0) {
+				
+			}
+		}
+	}
+}
