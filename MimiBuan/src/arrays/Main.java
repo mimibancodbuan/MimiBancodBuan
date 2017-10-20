79fca56359c20cd33ed4169c9477f8486b449500
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-10-20 11:20:29 -0400

    moar arrayz

--- MimiBuan/src/arrays/Main.java
+++ MimiBuan/src/arrays/Main.java
@@ -1,9 +1,59 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ObjectArrays oA = new ObjectArrays();
+		// ObjectArrays oA = new ObjectArrays();
+		Person s = new Person("Ben", "Nockles", Borough.NY_BOROUGHS[0]);
+		int x = 10;
+		int[] a = {5,6,7};
+		test2(s, x, a);
+		System.out.println(s + ", " + x + ", " + Arrays.toString(a));
 	}
 
+	/*
+	 * PASS-BY-VALUE EXAMPLES
+	 * THIS DOES NOT CHANGE THE NAME
+	*/
+	public static void test1(Person s, int x, int[] arr) {
+		String name = s.getFirstName();
+		name = "Ilona";
+		x = 5;
+		arr = new int [3];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+	}
+	
+	/*
+	 * PASS-BY-VALUE EXAMPLES
+	 * THIS DOES CHANGE THE NAME
+	*/
+	public static void test2(Person s, int x, int[] arr) {
+		s.setFirstName("Ilona");
+		x = 5;
+		arr = new int [3];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+	}
+	
+	/*
+	 * PASS-BY-VALUE EXAMPLES
+	 * THIS DOES CHANGE THE NAME
+	 * THIS ALSO CHANGES THE ARRAY
+	 * NO METHOD CAN PERMANENTLY CHANGE AND INT
+	 * (you can if you set it as a return type)
+	*/
+	public static void test3(Person s, int x, int[] arr) {
+		s.setFirstName("Ilona");
+		x = 5;
+		// arr = new int [3]; AS LONG AS YOU DON'T MAKE A 'NEW' ARRAY
+		// THE REFERENCES THEMSELVES WILL CHANGE
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+	}
 }
--- MimiBuan/src/arrays/Person.java
+++ MimiBuan/src/arrays/Person.java
@@ -15,6 +15,7 @@ public class Person {
 	private Borough home;
 	private Hobby hobby;
 	private Person[] friends;
+	private String nickname;
 	
 	public Person(String first, String last, Borough home){
 		this.firstName = first;
@@ -22,8 +23,48 @@ public class Person {
 		this.home = home;
 		this.hobby = Hobby.randomHobby();
 		friends = new Person[3];
+		this.nickname = createNickname(firstName);
 	}
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		nickname = createNickname(firstName);
+	}
+
+
+
+	// JAVA IS PASS-BY-VALUE
+	// meaning the parameters of a method are just values, not references
+	// so if you change those values, the original object is not affected.
+	// In this case, 'name' will not be changed. In fact, NOTHING can change.
+	// this.firstName via name
+	public static String createNickname(String name) {
+		String nickname = "";
+		int secondVowelPsn = findSecond(name);
+		nickname = name.substring(0, secondVowelPsn);
+		return nickname;
+	}
+	
+	private static int findSecond(String word) {
+		boolean foundVowel = false;
+		int index = word.length();
+		for(int i = 0; i < word.length(); i++) {
+			if(word.substring(i, i + 1).equals("a") || word.substring(i, i + 1).equals("e") || word.substring(i, i + 1).equals("i") || word.substring(i, i + 1).equals("o") || word.substring(i, i + 1).equals("u")) {
+				if(!foundVowel)
+					foundVowel = true;
+				else
+					return i;
+			}
+		}
+		return index;
+	}
+
 	/*
 	 * chooses friends from people based on who is of the same class
 	 * as this instance and who has the same hobbies
@@ -83,7 +124,7 @@ public class Person {
 	}
 	
 	public String toString() {
-		return "My name is " + firstName + " " + lastName + " and I live in " + home + ". I like to " + hobby;
+		return "My name is " + firstName + " " + lastName + ". Call me " + nickname + ". I live in " + home + ". I like to " + hobby;
 	}
 	
 }
